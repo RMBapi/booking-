b5f0b6fe2df5edd213154bc12a9a080f288185d5
bapi <rafid.bapi@pathao.com>
2026-04-06 23:51:03 +0600

    fix render

--- src/main.ts
+++ src/main.ts
@@ -134,7 +134,7 @@ async function bootstrap() {
     }
 
     const port = process.env.PORT ?? 3000;
-    await app.listen(port);
+    await app.listen(port, '0.0.0.0');
 
     if (!isProd) {
       logger.log(`Application is running on: http://localhost:${port}`);
